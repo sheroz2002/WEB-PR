42fb2922ef846fa1141b9435e02b170c109b9fe5
sheroz2002 <91754299+sheroz2002@users.noreply.github.com>
2022-09-27 18:18:58 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(window).on('load', function(){
+    // vide.js - video background
+    $('#ppr').vide('./video/cover',{
+        bgcolor:'#64947b'
+    })
+});
+$(window).on('load', function(){
+    // vide.js - video background
+    $('#header').vide('./video/pr.webm',{
+        bgcolor:'#64947b'
+    })
+});
